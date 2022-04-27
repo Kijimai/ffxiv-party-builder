6aaebc156ad17598e468c7b9e74c8ccb758235b2
John Deen Bucog <JDBucog@yahoo.com>
2022-04-27 01:15:47 -0700

    added search characters functionality, figure out how to use jinja to make use of the ID we pass in to fetch the single character

---
+++ flask_app/static/js/search_characters.js
@@ -0,0 +1,74 @@
+const searchCharactersForm = document.getElementById("search-characters")
+
+searchCharactersForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const characterName = e.target[0].value
+  const serverName = e.target[1].value
+  console.log(e)
+  console.log(characterName, serverName)
+  searchCharacters(characterName, serverName)
+})
+
+async function searchCharacters(characterName, serverName = "") {
+  isLoading = true
+  // if (isLoading) {
+  //   displayUsers.innerHTML = `
+  //     <div>
+  //       <h2>Loading!!!!</h2>
+  //     </div>
+  //   `
+  // }
+  if (!serverName) {
+    characters = await fetch(
+      `https://xivapi.com/character/search?name=${characterName}&extended=1`
+    )
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        // console.info(data.Results)
+        console.log(data)
+        console.log(data.Results)
+        renderResults(data.Results)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  } else {
+    characters = await fetch(
+      `https://xivapi.com/character/search?name=${characterName}&server=${serverName}`
+    )
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        console.log(data)
+        console.log(data.Results)
+        renderResults(data.Results)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  isLoading = false
+}
+
+function renderResults(list) {
+  let output = ""
+  const searchResults = document.getElementById("search_results_container")
+  list.map((character) => {
+    console.log(character)
+    output += `
+    <article>
+      <img src=${character.Avatar} alt=${character.Name} />
+      <form>
+      <p>${character.Name}</p>
+        <a href="/characters/${character.ID}">View Character</a>
+      </form>
+    </article>
+    `
+  })
+  searchResults.innerHTML = output
+}
+
+//Trouble sending specific ID from API call to Flask, to later render in Jinja by using that same ID to call the API using Jinja
